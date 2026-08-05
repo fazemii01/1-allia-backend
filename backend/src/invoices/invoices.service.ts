f27bf0d5f15830655d86ef06ca41543fb0358f15
fazemii01 <bestvatp@gmail.com>
2026-08-05 13:58:01 +0700

    bck19

--- backend/src/invoices/invoices.service.ts
+++ backend/src/invoices/invoices.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, ILike } from 'typeorm';
 import { Invoice } from './entities/invoice.entity';
 import { CreateInvoiceDto } from './dto/create-invoice.dto';
 
